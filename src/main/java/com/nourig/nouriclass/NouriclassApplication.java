1eb705b595eb1d1d771fd7fb1e171575048ca5d8
hee_!J <heeej920@naver.com>
2024-08-09 10:28:02 +0900

    feat:spring boot 세팅 #1

---
+++ src/main/java/com/nourig/nouriclass/NouriclassApplication.java
@@ -0,0 +1,13 @@
+package com.nourig.nouriclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NouriclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NouriclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nourig/nouriclass/NouriclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourig.nouriclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NouriclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
